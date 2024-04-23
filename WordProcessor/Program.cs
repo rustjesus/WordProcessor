29a6aa89b10b83aa3ec02ee8b2c12d7a0ae94508
rustjesus <yeric12345@gmail.com>
2024-04-22 23:13:16 -0500

    fix

--- WordProcessor/Program.cs
+++ WordProcessor/Program.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace WordProcessor
 {
-    public class MainProgram : Form
+    public class MyForm : Form
     {
         static List<string> dictionary = new List<string>();
         [STAThread]
@@ -41,7 +41,7 @@ namespace WordProcessor
 
         private TextBox replacebox2;
         private Label replacelabel2;
-        public MainProgram()
+        public MyForm()
         {
             this.Text = "Text File Modder";
             this.ClientSize = new Size(800, 600);
